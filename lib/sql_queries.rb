3c3cfafe2ce315f66d6be6751101fafd008d2e4f
Sophie Gu <sophieqgu@gmail.com>
2019-06-12 19:01:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,7 +24,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "SELECT projects.title, SUM(pledges.amount) AS total_pledged FROM projects
  JOIN pledges ON projects.id = pledges.project_id 
  GROUP BY projects.title 
- HAVING total_pledged > projects.funding_goal"
+ HAVING total_pledged >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
